0db37148474fd74f8dcfb51b3b5ef7a3f5f9f098
Elena M <elenavmala@gmail.com>
2022-04-30 13:37:58 -0500

    Create event list

--- football.js
+++ football.js
@@ -85,3 +85,34 @@ for (const player of game.scored) {
   scores[player] ? (scores[player] += 1) : (scores[player] = 1);
 }
 console.log(scores);
+
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
+
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+gameEvents.delete(64);
+console.log(gameEvents);
+
+for (const [key, value] of gameEvents) {
+  const half = key < 45 ? "FIRST" : "SECOND";
+  console.log(`[${half} HALF] ${key}: ${value}`);
+  // if (key < 45) {
+
+  //   //key < 45 ? console.log(`[FIRST HALF] ${key}: ${value}`)
+  //   console.log(`[FIRST HALF] ${key}: ${value}`);
+  // } else {
+  //   console.log(`[SECOND HALF] ${key}: ${value}`);
+  // }
+}
